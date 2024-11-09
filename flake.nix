c53914bba714504ca9bec757f7d1373e52046498
haruki7049 <tontonkirikiri@gmail.com>
2024-11-10 08:19:29 +0900

    Init rust toolchain

--- flake.nix
+++ flake.nix
@@ -2,12 +2,17 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
+    crane.url = "github:ipetkov/crane";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -21,9 +26,39 @@
         inputs.treefmt-nix.flakeModule
       ];
 
-      perSystem = { pkgs, ... }: {
+      perSystem = { pkgs, system, ... }:
+      let
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        craneLib = (inputs.crane.mkLib pkgs).overrideToolchain rust;
+        overlays = [ inputs.rust-overlay.overlays.default ];
+        src = craneLib.cleanCargoSource ./.;
+        cargoArtifacts = craneLib.buildDepsOnly {
+          inherit src;
+        };
+        flashing = craneLib.buildPackage {
+          inherit src cargoArtifacts;
+          strictDeps = true;
+
+          doCheck = true;
+        };
+      in
+      {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system overlays;
+        };
+
+        packages = {
+          inherit flashing;
+          default = flashing;
+        };
+
+        checks = {
+          inherit flashing;
+        };
+
         devShells.default = pkgs.mkShell {
           packages = [
+            rust
             pkgs.nil
           ];
         };
